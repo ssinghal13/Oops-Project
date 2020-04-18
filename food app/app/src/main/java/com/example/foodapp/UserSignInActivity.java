714b934365fb43a8a0c8be5601a4c14c2e99c819
ssinghal13 <40996769+ssinghal13@users.noreply.github.com>
2020-04-19 01:41:52 +0530

    Update UserSignInActivity.java

--- food app/app/src/main/java/com/example/foodapp/UserSignInActivity.java
+++ food app/app/src/main/java/com/example/foodapp/UserSignInActivity.java
@@ -46,7 +46,7 @@ public class UserSignInActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String txt_email=email_id.getText().toString().trim();
-                String txt_password=email_id.getText().toString().trim();
+                String txt_password=password.getText().toString().trim();
 
                 if( TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
                     Toast.makeText(UserSignInActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
