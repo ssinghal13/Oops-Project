9a7f9aba55a1fac928a3a20c90ad5be1287b24a3
ssinghal13 <40996769+ssinghal13@users.noreply.github.com>
2020-04-22 01:45:04 +0530

    corrected OTP

--- food app/app/src/main/java/com/example/foodapp/RiderViewDetailsActivity.java
+++ food app/app/src/main/java/com/example/foodapp/RiderViewDetailsActivity.java
@@ -92,7 +92,7 @@ public class RiderViewDetailsActivity extends AppCompatActivity {
                         OtpItem u= dataSnapshot.getValue(OtpItem.class);
                         HashMap<String, Object> otp=new HashMap<>();
                         otp.put("OTP", "YES");
-                        userRef.updateChildren(otp);
+                        otpRef.updateChildren(otp);
 
                     }
 
