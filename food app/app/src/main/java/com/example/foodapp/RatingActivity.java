11dc83b4cbdcbeafae6e38f04572e702875644be
ssinghal13 <40996769+ssinghal13@users.noreply.github.com>
2020-04-25 21:58:32 +0530

    changes

--- food app/app/src/main/java/com/example/foodapp/RatingActivity.java
+++ food app/app/src/main/java/com/example/foodapp/RatingActivity.java
@@ -10,5 +10,9 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+
+        
+
+
     }
 }
